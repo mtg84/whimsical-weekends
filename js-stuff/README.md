97b32138f96adc7ab29a4a97c268e24aa062ddbb
Maria <formacionjee@gmail.com>
2021-02-28 18:20:42 +0100

    Add js stuff

--- js-stuff/README.md
+++ js-stuff/README.md
@@ -1,3 +1,11 @@
-# moreless
-[Add a READ MORE Hidden-Text Drop-Down in simple JavaScript, HTML and CSS](https://www.youtube.com/watch?v=kQW-MXriUIU)
+27/02/2021
 
+# moreless
+Code with Ania Kubów: [Add a READ MORE Hidden-Text Drop-Down in simple JavaScript, HTML and CSS](https://www.youtube.com/watch?v=kQW-MXriUIU)
+
+<hr>
+
+28/02/2021
+
+# hoverbgchange
+Web Cifar: [On Hover Background Change With HTML, CSS & JS | Modern Hero Section](https://www.youtube.com/watch?v=qDxlBe2OhJU)
---
+++ js-stuff/hoverbgchange/app.js
@@ -0,0 +1,29 @@
+const parentBgEl = document.querySelector('.bg');
+const childEls = document.querySelectorAll('.container .text span');
+const textEl = document.querySelector('.text');
+const lineEl = document.querySelector('#line');
+
+childEls.forEach( el => {
+
+    el.addEventListener('mouseover', (e) =>{
+        const id = el.getAttribute('data-id');
+        const bgEl  = parentBgEl.querySelector(`.id-${id}`);
+
+        parentBgEl.querySelectorAll('img').forEach( img => {
+            img.style.display = 'none';
+            img.style.animation = '';
+        });
+
+        bgEl.style.display = 'block';
+        bgEl.style.animation = 'anima 1s ease forwards';
+
+
+        // line animation
+        const coords = el.getBoundingClientRect();
+        const textCoords = textEl.getBoundingClientRect();
+        line.style.width = coords.width + 'px';
+        line.style.left = coords.left - textCoords.left + 'px';
+    
+    });
+
+});
